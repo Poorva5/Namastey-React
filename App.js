24be294e1d2883b0665f33275ed8a6a91e9ceae1
Poorva5 <poorvak455@gmail.com>
2023-07-12 11:15:52 +0530

    Added header of website

--- App.js
+++ App.js
@@ -1,39 +1,59 @@
-const parent = React.createElement(
-    "div", 
-    {id: "parent"},
-    [React.createElement(
-        "div", 
-        {id: "child"}, 
-        [React.createElement(
-            "h1",
-            {},
-            "I am a h1 tag"
-        ),
-        React.createElement(
-            "h2",
-            {},
-            "I am a h2 tag"
-        )]
-    ),
-    React.createElement(
-        "div", 
-        {id: "child2"}, 
-        [React.createElement(
-            "h1",
-            {},
-            "I am a h1 tag"
-        ),
-        React.createElement(
-            "h2",
-            {},
-            "I am a h2 tag"
-        )]
-    )
-]
-)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
+const styleCard = {
+    backgroundColor: '#f0f0f0'
+}
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='logo-container'>
+            <img className='logo' src="https://cdn.dribbble.com/users/3055978/screenshots/11864513/1_4x.jpg"/>
+            </div>
+            <div className='nav-items'>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const RestaurantCard = () => {
+    return (
+        <div className='res-card' style={styleCard}>
+            <img className='res-logo' alt="res-logo" src="https://cookingfromheart.com/wp-content/uploads/2017/09/Veg-Biryani-in-Pressure-Cooker-6.jpg"/>
+            <h3>Meghna Foods</h3>
+        </div>
+    )
+}
+
+
+const Body = () => {
+    return (
+        <div className='body'>
+            <div className='search'>Search</div>
+            <div className='res-container'>
+                <RestaurantCard/>
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className='app'>
+            <Header/>
+            <Body/>
+        </div>
+    )
+}
 
 const root  = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent)
+root.render(<AppLayout/>)
 
